5db7f8fe9231385efcc80d8a180fc035fb05e348
jkinman <joel.kinman@gmail.com>
2018-04-02 19:49:11 -0700

    add bootstrap 4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, {Component} from "react";
-import {Provider} from "react-redux";
+import React, { Component } from "react";
+import { Provider } from "react-redux";
 
 import configureStore from "./stores";
 
+import "bootstrap";
 import "./App.css";
 import "./styles/app.scss";
 
@@ -11,18 +12,25 @@ import PollEthereum from "./components/smart/PollEthereum";
 const store = configureStore();
 
 class App extends Component {
-		render() {
-				return (
-						<Provider store={store}>
-								<div className="App">
-										<header className="App-header">
-												<h1 className="App-title">Welcome to Ethereum React Starter Kit</h1>
-										</header>
-										<PollEthereum/>
-								</div>
-						</Provider>
-				);
-		}
+  render() {
+    return (
+      <Provider store={store}>
+        <div className="App">
+          <div className="container">
+            <header className="App-header">
+              <h1 className="App-title">
+                Welcome to Ethereum React Starter Kit
+              </h1>
+              <h2>
+                This is a suite of tools to help digest the ethereum blockchain
+              </h2>
+            </header>
+            <PollEthereum />
+          </div>
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
